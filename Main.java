e2f7e95ee18f35b58d7bc3997b9378a2db95bab4
Nazmul Hoda <52332959+Nazmulhoda1212@users.noreply.github.com>
2019-07-18 11:56:36 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,81 @@
- package agecalculator;
+package converter;
 
-import java.time.LocalDate;
-import java.time.Period;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    @SuppressWarnings("empty-statement")
-    public void start(Stage primaryStage) {
-        Text msg = new Text("0 years 0 months 0 days");
-        DatePicker dp = new DatePicker();
-        Text format = new Text("MM/dd/yyyy");
-        dp.setValue(LocalDate.now());
-        Button age = new Button();
-        age.setText("Calculate");
-        age.setOnAction((ActionEvent event) -> {
-            Period p = ageCalculator(dp.getValue());
-            msg.setText(p.getYears() + " years " + p.getMonths() + " months " + p.getDays() + " days");
-        });
-        VBox vb = new VBox(5, format, dp, age, msg);
-        vb.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(vb, 300, 250);
-        primaryStage.setTitle("Age Calculator");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    Period ageCalculator(LocalDate birthDay) {
-        return Period.between(birthDay, LocalDate.now());
-    }
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        
+        
     }
 
+    static double convert(double value, String from, String to) {
+        switch (from) {
+            case "Kilometre":
+                value *= 1000;
+                break;
+            case "Centimetre":
+                value /= 100;
+                break;
+            case "Mile":
+                value *= 1609.344;
+                break;
+            case "Foot":
+                value /= 3.281;
+                break;
+            case "Inch":
+                value /= 39.37;
+                break;
+            case "Gram":
+                value /= 1000;
+                break;
+            case "Pound":
+                value /= 2.205;
+                break;
+            case "Tonne":
+                value *= 1000;
+                break;
+            case "Fahrenheit":
+                value = (value - 32) * 5 / 9;
+                break;
+            case "Kelvin":
+                value -= 273.15;
+                break;
+            case "Second":
+                value /= 60;
+                break;
+            case "Hour":
+                value *= 60;
+                break;
+            case "Day":
+                value *= 1440;
+        }
+        switch (to) {
+            case "Kilometre":
+                return value / 1000;
+            case "Centimetre":
+                return value * 100;
+            case "Mile":
+                return value / 1609.344;
+            case "Foot":
+                return value * 3.281;
+            case "Inch":
+                return value * 39.37;
+            case "Gram":
+                return value * 1000;
+            case "Pound":
+                return value * 2.205;
+            case "Tonne":
+                return value / 1000;
+            case "Fahrenheit":
+                return (value * 9 / 5) + 32;
+            case "Kelvin":
+                return value + 273.15;
+            case "Second":
+                return value * 60;
+            case "Hour":
+                return value / 60;
+            case "Day":
+                return value / 1440;
+        }
+        return value;
+    }
 }
